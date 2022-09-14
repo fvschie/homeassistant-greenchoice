f4628b25ea6aa2f978a5d6aaee7a21c9d5c0396b
Frank <frank@nebulous.nl>
2022-09-14 22:34:04 +0200

    Only create sensors for products (power or gas) which are available, including tariffs.

--- custom_components/greenchoice/config_flow.py
+++ custom_components/greenchoice/config_flow.py
@@ -75,12 +75,20 @@ class GreenchoiceFlowHandler(ConfigFlow, domain=DOMAIN):
         """Handle setup flow Greenchoice sensor."""
         errors = {}
 
-        print(f"Checking if we have user input: {user_input}")
         if user_input is not None:
             self.data[CONF_OVEREENKOMST_ID] = user_input[CONF_OVEREENKOMST_ID]
             await self.async_set_unique_id(user_input[CONF_OVEREENKOMST_ID])
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=f"Greenchoice ({user_input[CONF_OVEREENKOMST_ID]})", data=self.data)
+            products = await self.hass.async_add_executor_job(self.api.get_products, int(user_input[CONF_OVEREENKOMST_ID]))
+            self.data["has_power"] = products.has_power
+            self.data["has_gas"] = products.has_gas
+            default_options = {
+                CONF_SCAN_INTERVAL: DEFAULT_SCAN_INTERVAL_MINUTES,
+                CONF_METERSTAND_STROOM_ENABLED: self.data["has_power"],
+                CONF_METERSTAND_GAS_ENABLED: self.data["has_gas"],
+                CONF_TARIEVEN_ENABLED: True,
+            }
+            return self.async_create_entry(title=f"Greenchoice ({user_input[CONF_OVEREENKOMST_ID]})", data=self.data, options=default_options)
 
         overeenkomsten = await self.hass.async_add_executor_job(self.api.get_overeenkomsten)
         options = list[SelectOptionDict]()
@@ -116,8 +124,8 @@ class GreenchoiceSensorOptionsFlowHandler(OptionsFlow):
             # Convert scan interval to integer
             options_data = {
                 CONF_SCAN_INTERVAL: int(user_input[CONF_SCAN_INTERVAL]),
-                CONF_METERSTAND_STROOM_ENABLED: user_input[CONF_METERSTAND_STROOM_ENABLED],
-                CONF_METERSTAND_GAS_ENABLED: user_input[CONF_METERSTAND_GAS_ENABLED],
+                CONF_METERSTAND_STROOM_ENABLED: user_input.get(CONF_METERSTAND_STROOM_ENABLED, False),
+                CONF_METERSTAND_GAS_ENABLED: user_input.get(CONF_METERSTAND_GAS_ENABLED, False),
                 CONF_TARIEVEN_ENABLED: user_input[CONF_TARIEVEN_ENABLED]
             }
             return self.async_create_entry(title="", data=options_data)
@@ -126,17 +134,21 @@ class GreenchoiceSensorOptionsFlowHandler(OptionsFlow):
         options.append(SelectOptionDict(value="60", label="elk uur"))
         options.append(SelectOptionDict(value="1440", label="elke dag"))
         options.append(SelectOptionDict(value="10080", label="elke week"))
-        schema = vol.Schema({
+
+        schema = {
             vol.Optional(
                 CONF_SCAN_INTERVAL,
                 default=str(self.config_entry.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL_MINUTES))
-            ): SelectSelector(SelectSelectorConfig(options=options, mode=SelectSelectorMode.LIST)),
-            vol.Required(CONF_METERSTAND_STROOM_ENABLED, default=self.config_entry.options.get(CONF_METERSTAND_STROOM_ENABLED, DEFAULT_METERSTAND_STROOM_ENABLED)): bool,
-            vol.Required(CONF_METERSTAND_GAS_ENABLED, default=self.config_entry.options.get(CONF_METERSTAND_GAS_ENABLED, DEFAULT_METERSTAND_GAS_ENABLED)): bool,
-            vol.Required(CONF_TARIEVEN_ENABLED, default=self.config_entry.options.get(CONF_TARIEVEN_ENABLED, DEFAULT_TARIEVEN_ENABLED)): bool,
-        })
+            ): SelectSelector(SelectSelectorConfig(options=options, mode=SelectSelectorMode.LIST))
+        }
+        data = self.config_entry.data
+        if data["has_power"]:
+            schema[vol.Required(CONF_METERSTAND_STROOM_ENABLED, default=self.config_entry.options.get(CONF_METERSTAND_STROOM_ENABLED, DEFAULT_METERSTAND_STROOM_ENABLED))] = bool
+        if data["has_gas"]:
+            schema[vol.Required(CONF_METERSTAND_GAS_ENABLED, default=self.config_entry.options.get(CONF_METERSTAND_GAS_ENABLED, DEFAULT_METERSTAND_GAS_ENABLED))] = bool
+        schema[vol.Required(CONF_TARIEVEN_ENABLED, default=self.config_entry.options.get(CONF_TARIEVEN_ENABLED, DEFAULT_TARIEVEN_ENABLED))] = bool
 
         return self.async_show_form(
             step_id="init",
-            data_schema=schema,
+            data_schema=vol.Schema(schema),
         )
